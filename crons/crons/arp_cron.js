e40612dedc37de5ff5478ae196f5c8f6b9fa5cba
Wouter Versyck <wouter.versyck@gmail.com>
2019-04-19 21:46:22 +0200

    Added arps-scan to cron job

--- crons/crons/arp_cron.js
+++ crons/crons/arp_cron.js
@@ -1,7 +1,15 @@
 const CronJob = require('cron').CronJob;
-const job = new CronJob('* * * * * *', function() {
+const arpScanner = require('arpscan/promise');
+
+const job = new CronJob('0 */5 * * * *', function() {
     const d = new Date();
     console.log('At Ten Minutes:', d);
+
+    arpScanner({sudo: true}).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
 
 module.exports = job;
